e12d2b096d2a91a41158bc833abc60ef4579d2e4
Ivan Zenov <ivanzenov07@gmail.com>
2019-11-08 15:59:19 +0300

    correct second lab

--- src/by/bsu/TriangleInequalityTest.java
+++ src/by/bsu/TriangleInequalityTest.java
@@ -5,55 +5,53 @@ import org.junit.Test;
 import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertFalse;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TriangleInequalityTest {
     @Test
-    public void rightTriangle() {
-        assertTrue(TriangleInequality.isTriangleExists(5.0, 4.0, 3.0));
+    public void rightTriangle(){
+        assertTrue(TriangleInequality.isTriangleExist(5.0, 4.0, 3.0));
     }
 
     @Test
     public void allZeroSides() {
-        assertFalse(TriangleInequality.isTriangleExists(0.0, 0.0, 0.0));
+        assertThrows(IllegalArgumentException.class, () -> TriangleInequality.isTriangleExist(0, 0, 0));
     }
 
     @Test
     public void oneZeroSide() {
-       assertFalse(TriangleInequality.isTriangleExists(0.0,4.0,5.0));
+        assertThrows(IllegalArgumentException.class, () -> TriangleInequality.isTriangleExist(0.0, 4.0, 5.0));
     }
 
     @Test
     public void oneNegativeSide() {
-        assertFalse(TriangleInequality.isTriangleExists(-3.0, 4.0, 5.0));
+        assertThrows(IllegalArgumentException.class, () -> TriangleInequality.isTriangleExist(-3.0, 4.0, 5.0));
     }
 
     @Test
     public void allNegativeSides() {
-        assertFalse(TriangleInequality.isTriangleExists(-3.0, -4.0, -5.0));
+        assertThrows(IllegalArgumentException.class, () -> TriangleInequality.isTriangleExist(-3.0, -4.0, -5.0));
+
     }
 
     @Test
     public void sumOfTwoSidesLessThird() {
-        assertFalse(TriangleInequality.isTriangleExists(3.0, 3.0, 6.1));
+        assertFalse(TriangleInequality.isTriangleExist(3.0, 3.0, 6.1));
     }
 
     @Test
-    public void sumOfTwoSidesEqualsThird() {
-        assertFalse(TriangleInequality.isTriangleExists(12.5, 12.5, 25.0));
+    public void sumOfTwoSidesEqualsThird()  {
+        assertFalse(TriangleInequality.isTriangleExist(12.5, 12.5, 25.0));
     }
 
     @Test
     public void sumOfTwoSidesGreaterThird() {
-        assertFalse(TriangleInequality.isTriangleExists(12.6, 99.3, 24.7));
+        assertFalse(TriangleInequality.isTriangleExist(12.6, 99.3, 24.7));
     }
 
     @Test
-    public void allEqualSide() {
-        assertTrue(TriangleInequality.isTriangleExists(5.0, 5.0, 5.0));
-    }
-
-    @Test
-    public void oneSideIsMaxDouble() {
-        assertFalse(TriangleInequality.isTriangleExists(Double.MAX_VALUE,5.0,4.0));
+    public void allEqualSide(){
+        assertTrue(TriangleInequality.isTriangleExist(5.0, 5.0, 5.0));
     }
 
 
